54ccc80d209497b85f7f91cda6e5cac3cfb4affe
Posoroko <eric@posoroko;com>
2023-07-14 09:31:27 +0200

    galery modal

---
+++ components/Widget/LeftRightNavigation.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+const emit = defineEmits(['navigate'])
+
+function navigate(e) {
+    emit('navigate', e.currentTarget.dataset.direction)
+}
+</script>
+
+<template>
+    <div class="absolute full top0 left0 noEvents_kidsEvents">
+        <span class="icon chevron left" @click="navigate" data-direction="left">chevron_left</span>
+
+        <span class="icon chevron right" @click="navigate" data-direction="right">chevron_right</span>
+    </div>
+</template>
+
+<style scoped>
+.chevron {
+    width: 48px;
+    height: 48px;
+    color: white;
+    font-size: 40px;
+    line-height: 1;
+    background-color: rgba(0, 0, 0, 0.419);
+    display: grid;
+    place-items: center;
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.chevron.left {
+    left: 10px;
+}
+.chevron.right {
+    right: 10px;
+}
+
+</style>
--- pages/galerie.vue
+++ pages/galerie.vue
@@ -1,3 +1,113 @@
+<script setup>
+const route = useRoute()
+
+const appConfig = useAppConfig();
+const directusAssets = appConfig.directus.assets;
+const directusItems = appConfig.directus.items;
+
+const fetchOptions = {
+    server: true,
+    params: {
+        // fields: 'id, title, poster, teaser, slug',
+    }
+}
+
+const { data: images } = await useAsyncData(
+    "galery",
+    async () => {
+        const items = await $fetch(`${directusItems}Galery`, fetchOptions)
+
+        return items.data
+    }
+    ,
+    { server: true }
+)
+
+const selectedImageIndex = ref(null)
+
+const modalIsOpen = ref(false)
+
+function handleImageclick(e) {
+    selectedImageIndex.value = e.target.dataset.index
+    modalIsOpen.value = true
+}
+function closeModal() {
+    modalIsOpen.value = false
+}
+
+function navigate(direction) {
+    console.log(selectedImageIndex.value)
+    if(direction === 'left' && selectedImageIndex.value > 0) {
+        selectedImageIndex.value--
+    } else if(direction === 'right' && selectedImageIndex.value < images.value.length - 1){
+        selectedImageIndex.value++
+    }
+}
+
+</script>
+
 <template>
-    <h1>hello galerie</h1>
+    <PageMain>
+        <template #headerImage>
+
+        </template>
+
+        <template #main>
+
+            <img class="pointer" 
+                v-for="(image, index) in images" 
+                :src="`${directusAssets}${image.image}`" 
+                :alt="`${image.alt}`"
+                :data-index="index"
+                @click="handleImageclick">
+
+            <div class="modal r" v-if="modalIsOpen">
+                <div class="modalContent centered pad20">
+                    <span class="close lightText" @click="closeModal">&times;</span>
+
+                    <img class="objectFitContain" :src="`${directusAssets}${images[selectedImageIndex].image}`" :alt="images[selectedImageIndex].alt">
+
+                    <WidgetLeftRightNavigation @navigate="navigate" />
+                </div>
+            </div>    
+        </template>
+
+    </PageMain>
 </template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    padding: max(5vw, 10px);
+    background-color: #0000005d;
+    backdrop-filter: blur(5px);
+}
+.modalContent {
+    width: 100%;
+    height: 100%;
+    background-color: var(--background);
+    border-radius: 10px;
+    box-shadow: 1px 1px 10px #00000097;
+    position: relative;
+}
+.close {
+    width: 48px;
+    height: 48px;
+    font-size: 40px;
+    line-height: 1;
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: grid;
+    place-items: center;
+}
+.close:hover {
+    rotate: 360deg;
+    transition: 300ms ease;
+}
+</style>
